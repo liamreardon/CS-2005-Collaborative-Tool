07a8502632b17b8590e56f55716036cd2aa64ba7
Liam Reardon <lmr647@mun.ca>
2018-03-10 02:06:18 -0330

    create login branch

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
 # --- Imports ---
 from flask import render_template, session, redirect, url_for, request
+import os
 # --- Custom imports ---
 from app import app
 from app.models import *
@@ -8,3 +9,4 @@ from app.models import *
 @app.route('/')
 def index():
     return render_template("index.html", title="Example Title", text="Hello: World")
+
