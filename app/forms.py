84aafbbd192655b9683f6d1cb42772b1343d80f5
Liam Reardon <lmr647@mun.ca>
2018-03-12 19:33:46 -0230

    fix password length for login

--- app/forms.py
+++ app/forms.py
@@ -4,7 +4,7 @@ from wtforms.validators import InputRequired, Email, Length
 
 class LoginForm(FlaskForm):
 	username = StringField('username', validators=[InputRequired(), Length(min=4, max=20)])
-	password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=80)])
+	password = PasswordField('password', validators=[InputRequired(), Length(min=6, max=80)])
 	remember = BooleanField('remember me')
 
 class RegisterForm(FlaskForm):
