f9dd60421fd837d9340dd97ce9443b69c907be36
Liam Reardon <lmr647@mun.ca>
2018-03-12 19:24:03 -0230

    fix routes.py merge conflict

--- app/routes.py
+++ app/routes.py
@@ -12,7 +12,7 @@ from app.unit_testing import *
 @app.route('/')
 @app.route('/index')
 def index():
-<<<<<<< HEAD
+
     return render_template("index.html", title="Example Title", text="Hello: Dude")
 
 @app.route('/testing')
@@ -26,7 +26,7 @@ def testing():
     posts = make_posts(users)
     users = User.query.all()
     return render_template("unit_testing.html", title="Example Title", posts=posts)
-=======
+
     return render_template("index.html", title="Example Title", text="Hello: World")
->>>>>>> login
+
 
