27feab0ce3d1e0fba1909c3515b2abe0af74da9e
Liam Reardon <lmr647@mun.ca>
2018-03-13 19:16:44 -0230

    Link signup and login forms to database, add further security

---
+++ app/loaders.py
@@ -0,0 +1,11 @@
+from app import app
+from app.models import *
+from flask_login import LoginManager
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+@login_manager.user_loader
+def load_user(user_id):
+	return User.query.get(int(user_id))
--- app/routes.py
+++ app/routes.py
@@ -5,9 +5,13 @@ import os
 from app import app
 from app.models import *
 from app.forms import *
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import login_user, login_required, logout_user, current_user
+from app.loaders import *
 # --- BAD IMPORTS ---
 #todo: delete this
 from app.unit_testing import *
+from app.models import *
 
 
 @app.route('/')
@@ -34,7 +38,14 @@ def login():
     form = LoginForm()
 
     if form.validate_on_submit():
-        return '<h1>' + form.username.data + ' ' + form.password.data + '</h1>'
+        user = User.query.filter_by(username=form.username.data).first()
+        if user:
+            if check_password_hash(user.password, form.password.data):
+                login_user(user, remember=form.remember.data)
+                return redirect(url_for('dashboard'))
+
+        return '<h1>Invalid username or password</h1>'
+        #return '<h1>' + form.username.data + ' ' + form.password.data + '</h1>'
 
     return render_template('login.html', form=form)
 
@@ -43,12 +54,35 @@ def signup():
     form = RegisterForm()
 
     if form.validate_on_submit():
-        return '<h1>' + form.username.data + ' ' + form.email.data + ' ' + form.password.data + '</h1>'
+
+        hashed_password = generate_password_hash(form.password.data, method='sha256')
+        new_user = User(username=form.username.data, email=form.email.data, password=hashed_password)
+        db.session.add(new_user)
+        db.session.commit()
+        return '<h1>New user has been created.</h1>'
+        #return '<h1>' + form.username.data + ' ' + form.email.data + ' ' + form.password.data + '</h1>'
 
     return render_template('signup.html', form=form)
 
 @app.route('/dashboard')
+@login_required
 def dashboard():
-    return render_template('dashboard.html')
+    return render_template('dashboard.html', name=current_user.username)
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+
+
+
+
+
+
+
+
+
 
 
