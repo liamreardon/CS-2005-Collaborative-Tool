378e495f719c1749cdda3f35882de4c936449f4a
Liam Reardon <lmr647@mun.ca>
2018-03-19 00:27:41 -0230

    redirect user to login screen after signup

--- app/routes.py
+++ app/routes.py
@@ -72,8 +72,8 @@ def signup():
         new_user = User(username=form.username.data, email=form.email.data, password=hashed_password)
         db.session.add(new_user)
         db.session.commit()
-        return '<h1>New user has been created.</h1>'
-        #return '<h1>' + form.username.data + ' ' + form.email.data + ' ' + form.password.data + '</h1>'
+        return render_template('redirectSignup.html')
+           
 
     return render_template('signup.html', form=form)
 
