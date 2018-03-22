f6d27147ccc46272e07d4d4c0e6caecb58530e81
GilesZhang123 <37192257+GilesZhang123@users.noreply.github.com>
2018-03-22 17:01:07 -0230

    Update avatar URLs models.py

--- avatar URLs models.py
+++ avatar URLs models.py
@@ -5,5 +5,5 @@ class User(UserMixin, db.Model):
     # ...
     def avatar(self, size):
         digest = md5(self.email.lower().encode('utf-8')).hexdigest()
-        return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(
+        return 'https://www.cs2005group.com/avatar/{}?d=identicon&s={}'.format(
             digest, size)
