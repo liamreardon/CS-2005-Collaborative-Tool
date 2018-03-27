b5be0023f8204f89404c1b54676ea2e0027c5c92
tg-riff <37121325+tg-riff@users.noreply.github.com>
2018-03-27 17:44:55 -0230

    edit posts and edit threads complete
    if trouble, clear the database

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,5 @@
 # --- Imports ---
-<<<<<<< HEAD
 from flask import flash, render_template, session, redirect, url_for, request
-=======
-from flask import render_template, session, redirect, url_for, request, flash
->>>>>>> master
 import os
 # --- Custom imports ---
 from app import app
@@ -13,19 +9,16 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import login_user, login_required, logout_user, current_user
 from app.loaders import *
 # --- BAD IMPORTS ---
-# todo: delete this
+#todo: delete this
 from app.unit_testing import *
 from app.models import *
-
-
-# from app.forms import EditProfileForm
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template("index.html", title="Example Title", text="Hello: Dude")
 
+    return render_template("index.html", title="Example Title", text="Hello: Dude")
 
 @app.route('/testing')
 def testing():
@@ -33,153 +26,149 @@ def testing():
     Visiting this route will wipe the DB and perform some unit testing
     The objects will be printed to the site for debugging and validation
     """
-    # wipe DB and make dummy users
-    # init_db()
+    init_db()
     # users = make_users()
-    # # user0 creates a new thread
-    # topic1 = Topic("topic1")
-    # p1 = Post(users[0], "this is a test post #1 in thread #1", title="Thread #1 Title")
-    # thread1 = Thread(first_post=p1, topic=topic1)
-    # # users 1 and 2 reply
-    # p2 = Post(users[1], "this is a reply in thread1", thread1)
-    # p3 = Post(users[2], "this is a reply in thread1", thread1)
-    # p6 = Post(users[1], "this is my second post in thread1", thread1)
-    # thread1.add_post(p2)
-    # thread1.add_post(p3)
-    # thread1.add_post(p6)
-    # # user 3 posts a new thread with the same topic
-    # p4 = Post(users[3], "this is the first post in thread #2", title="Thread 2 title")
-    # thread2 = Thread(first_post=p4, topic=topic1)
-    # # user 4 will post a new thread with a new topic
-    # topic2 = Topic("topic2")
-    # p5 = Post(users[4], "this is a test post with a new topic", title="Thread 3 title")
-    # thread3 = Thread(p5, topic2)
-    return render_template("unit_testing.html", title="Example Title", posts=Post.query.all(), topics=Topic.query.all())
-
+    # posts = make_posts(users)
+    # threads = make_thread(users,posts)
+    # users = User.query.all()
+    return render_template("unit_testing.html", title="Example Title", posts=Post.query.all())
 
 @app.route('/create_thread', methods=['GET', 'POST'])
-# @login_required()
+#@login_required()
 def create_thread():
     """
-    Creates a new thread with a title and a new post with a body and
-    commits it to the database.
+    Create a new thread with a title and a new post
+    with a body and commits it to the database.
     """
     form = ThreadForm()
     if form.validate_on_submit():
-<<<<<<< HEAD
         new_thread = Thread()
         new_post = Post(title=form.thread.data,text=form.post.data,author_id=current_user.id,thread_id=new_thread.id)
         new_thread.add_first_post(new_post)
+        db.session.add(new_thread)
         db.session.commit()
         #flash('Thread submitted.')
         return redirect(url_for('view_threads'))
     return render_template('create_thread.html', form=form)
 
+@app.route('/add_topic/<string:id>', methods=['GET', 'POST'])
+#@login_required()
+def add_topic(id):
+    """
+    Create a new topic for a thread and commits
+    it to the database.
+    """
+    current_thread=Thread.query.get(id)
+
+    form = TopicForm()
+    if form.validate_on_submit():
+        new_topic = Topic(name=form.topic.data)
+        new_topic.add_thread(current_thread)
+        db.session.add(new_topic)
+        db.session.commit()
+        #flash('Thread submitted.')
+        return redirect(url_for('view_threads'))
+    return render_template('add_topic.html', form=form)
+
 @app.route('/view_threads', methods=['GET', 'POST'])
 #@login_required()
 def view_threads():
     """
-    Displays all the threads within the database into
-    a table and displays the id, title, author, and
-    datetime.
+    Insert all the threads within the database into
+    a table and display the id, title, author, and
+    datetime of each thread.
     """
-    threads = Thread.query.all()
+    threads = Thread.query.all()#topic.author.username
     return render_template('view_threads.html', threads=threads)
 
 @app.route('/view_thread/<string:id>', methods=['GET', 'POST'])
 #@login_required()
 def view_thread(id):
     """
-    Displays all the posts within a specific thread and includes
-    a form to insert a post within that thread.
+    Display all the posts within a thread and include
+    a form to create a new post within that thread.
     """
-    #posts = Post.query.filter_by(thread_id=id).all()
-    posts = Thread.query.filter_by(id=id).first().posts
+    posts = Post.query.filter_by(thread_id=id).all()
+    #posts = Thread.query.filter_by(id=id).first().posts
     current_thread = Thread.query.get(id)
 
     form = PostForm()
     if form.validate_on_submit():
-        new_post = Post(title=current_thread.topic,text=form.post.data,author_id=current_user.id,thread_id=current_thread.id)
+        new_post = Post(title=current_thread.name,text=form.post.data,author_id=current_user.id,thread_id=current_thread.id)
         current_thread.add_post(new_post)
         #flash('Post submitted.')
         return redirect(url_for('view_thread',id=id))
-    return render_template('view_thread.html', form=form, posts=posts)
+    return render_template('view_thread.html', form=form, posts=posts, current_thread=current_thread)
 
-@app.route('/edit_post/<string:id>', methods=['GET', 'POST'])
+@app.route('/view_thread/edit_post/<string:id>', methods=['GET', 'POST'])
 #@login_required()
 def edit_post(id):
     """
-    Users who have created posts can edit the posts they have created.
+    Identify a post created by the user and allow the user
+    to edit that post.
     """
     current_post = Post.query.get(id)
 
-    form = PostForm()
+    form = PostForm(post=current_post.text)
     if form.validate_on_submit():
-        new_post = Post(title=form.thread.data,text=form.post.data,author_id=current_user.id,thread_id=new_thread.id)
+        current_post.text = form.post.data
+        db.session.commit()
         #flash('Post editted.')
-        return redirect(url_for('view_thread',id=id))
-    return render_template('view_thread.html', form=form)
+        return redirect(url_for('view_thread', id=current_post.thread_id))
+    return render_template('edit_post.html', id=id, form=form, post=current_post)
 
 @app.route('/edit_thread/<string:id>', methods=['GET', 'POST'])
 #@login_required()
 def edit_thread(id):
     """
-    Users who have created threads can edit threads they have created.
+    Identify a thread created by the user and allow the user 
+    to edit that thread.
     """
     current_thread = Thread.query.get(id)
 
-    form = ThreadForm()
+    form = ThreadForm(thread=current_thread.name, post=current_thread.posts[0].text)
     if form.validate_on_submit():
-        new_thread = Thread()
-        new_post = Post(title=form.thread.data,text=form.post.data,author_id=current_user.id,thread_id=current_thread.id)
-        new_thread.add_first_post(new_post)
+        current_thread.name = form.thread.data
+        current_thread.posts[0].text = form.post.data
         db.session.commit()
         #flash('Thread editted.')
-        return redirect(url_for('view_thread',id=id))
-    return render_template('view_thread.html', form=form)
-=======
-        # new_thread = Thread(topic=form.thread.data)
-        new_post = Post(title=form.thread.data, text=form.post.data)
-        # db.session.add(new_thread)
-        db.session.add(new_post)
-        db.session.commit()
-        return '<h1>Thread submitted.</h1>'
-        # return '<h1>' + form.thread.data + form.post.data + '</h1>'
-    return render_template('create_thread.html', form=form)
-
->>>>>>> master
+        return redirect(url_for('view_threads', id=id))
+    return render_template('edit_thread.html', id=id, form=form, thread=current_thread)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
         if user:
             if check_password_hash(user.password, form.password.data):
                 login_user(user, remember=form.remember.data)
                 return redirect(url_for('home'))
-        return '<h1>Invalid username or password</h1>'
-    return render_template('login.html', form=form)
 
+        return '<h1>Invalid username or password</h1>'
+
+    return render_template('login.html', form=form)
 
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
-    form = RegistrationForm(request.form)
+    form = RegisterForm()
+
     if form.validate_on_submit():
+
         hashed_password = generate_password_hash(form.password.data, method='sha256')
         new_user = User(username=form.username.data, email=form.email.data, password=hashed_password)
         db.session.add(new_user)
         db.session.commit()
-        flash('Thanks for registering!')
-        return redirect(url_for('login'))
-    return render_template('signup.html', form=form)
+        return render_template('redirectSignup.html')
+           
 
+    return render_template('signup.html', form=form)
 
 @app.route('/home')
 @login_required
 def home():
     return render_template('home.html', name=current_user.username)
-
 
 @app.route('/logout')
 @login_required
@@ -188,57 +177,4 @@ def logout():
     return redirect(url_for('index'))
 
 
-@app.route('/subscriptions')
-@login_required
-def subscriptions():
-    return render_template('subscriptions.html')
 
-# region Profile
-
-@app.route('/user/<username>')
-@login_required
-def user(username):
-    user = User.query.filter_by(username=username).first_or_404()
-    posts = [
-        {'author': user, 'body': 'Test post #1'},
-        {'author': user, 'body': 'Test post #2'}
-    ]
-    return render_template('user.html', user=user, posts=posts)
-
-
-@app.route('/edit_profile', methods=['GET', 'POST'])
-@login_required
-def edit_profile():
-    form = EditProfileForm()
-    if form.validate_on_submit():
-        current_user.username = form.username.data
-        current_user.about_me = form.about_me.data
-        db.session.commit()
-        # flash('Your changes have been saved.') #flash not imported
-        return redirect(url_for('edit_profile'))
-    elif request.method == 'GET':
-        form.username.data = current_user.username
-        form.about_me.data = current_user.about_me
-
-    return render_template('edit_profile.html', title='Edit Profile',
-                           form=form)
-
-@app.route('/change_password', methods=['GET', 'POST'])
-def change_password():
-    form = ChangePasswordForm(request.form)
-    if form.validate_on_submit():
-        hashed_password = generate_password_hash(form.password.data, method='sha256')
-        current_user.password = hashed_password
-        db.session.commit()
-        flash('Your changes have been saved.')
-        return redirect(url_for('edit_profile'))
-    elif request.method == 'GET':
-        hashed_password = generate_password_hash(form.password.data, method='sha256')
-        hashed_password = current_user.password
-
-    return render_template('change_password.html', title='Change Password',
-                           form=form)
-
-
-
-# endregion
