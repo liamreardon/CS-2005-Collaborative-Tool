a95f67e8657c401ef61d187450d774a382fba53e
Rob B <infamousrob@gmail.com>
2018-03-29 16:54:44 -0230

    Incorporated notifications to master

--- app/routes.py
+++ app/routes.py
@@ -156,23 +156,11 @@ def view_topic(topic_name):
     return render_template('view_topic.html', threads=threads)
 
 
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    form = LoginForm()
-    if form.validate_on_submit():
-        user = User.query.filter_by(username=form.username.data).first()
-        if user:
-            if check_password_hash(user.password, form.password.data):
-                login_user(user, remember=form.remember.data)
-                return redirect(url_for('home'))
-        return '<h1>Invalid username or password</h1>'
-    return render_template('login.html', form=form)
-
-
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
     form = RegistrationForm(request.form)
     if form.validate_on_submit():
+        # todo bool checks for existing user and email
         hashed_password = generate_password_hash(form.password.data, method='sha256')
         new_user = User(username=form.username.data, email=form.email.data, password=hashed_password)
         db.session.add(new_user)
